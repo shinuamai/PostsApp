71ccb6c74ddd1cf06c777dc9787e5c8fd37f5ea8
shinuamai <laurad00sandovalg@gmail.com>
2024-01-31 11:50:41 -0500

    hook for requests

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+
+export function useFetch(url) {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const abortController = new AbortController();
+
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await fetch(url, { signal: abortController.signal });
+                const responseData = await response.json();
+                setData(responseData);
+            } catch (error) {
+                if (error.name === "AbortError") {
+                    console.log("Request Cancelled");
+                } else {
+                    setError(error);
+                }
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+
+        return () => abortController.abort();
+    }, [url]);
+
+    const handleRequest = async (method, id, body) => {
+        const abortController = new AbortController();
+        
+        try {
+            const response = await fetch(`${url}/${id}`, {
+                method: method,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body),
+                signal: abortController.signal
+            });
+            const responseData = await response.json();
+            return responseData;
+        } catch (error) {
+            if (error.name === "AbortError") {
+                console.log("Request Cancelled");
+            } else {
+                throw error;
+            }
+        } finally {
+            abortController.abort();
+        }
+    };
+
+    return { data, loading, error, handleRequest };
+}
