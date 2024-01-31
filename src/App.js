80e7673aa6f9a26f79e01a70f0d0684e9257ea4b
shinuamai <laurad00sandovalg@gmail.com>
2024-01-31 11:51:32 -0500

    Pages implements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Posts from './pages/Posts';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold text-pink-900">
-        Hello world
-      </h1>
-    </div>
+    <Router>
+      <div>
+        <nav className="shadow-md rounded-lg py-4 sm:px-10 lg:px-36 xl:px-40 px-4">
+          <div className="flex justify-between font-medium">
+            <h1 className='font-medium'>
+              POST APP
+            </h1>
+            <div>
+              <Link to="/" className="mr-3 hover:text-customBlue">HOME</Link>
+              <Link to="/posts" className="hover:text-customBlue">POSTS</Link>
+            </div>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts" element={<Posts />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
