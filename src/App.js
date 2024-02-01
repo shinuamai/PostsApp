863d1b15214c2f8450b1c824e66f52ff3ffb0086
shinuamai <laurad00sandovalg@gmail.com>
2024-02-01 12:19:14 -0500

    little changes in app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Posts from './pages/Posts';
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <div>
-        <nav className="shadow-md rounded-lg py-4 sm:px-10 lg:px-36 xl:px-40 px-4">
-          <div className="flex justify-between font-medium">
-            <h1 className='font-medium'>
-              POST APP
-            </h1>
-            <div>
-              <Link to="/" className="mr-3 hover:text-customBlue">HOME</Link>
-              <Link to="/posts" className="hover:text-customBlue">POSTS</Link>
-            </div>
-          </div>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/posts" element={<Posts />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Posts />} />
+      </Routes>
     </Router>
   );
 }
