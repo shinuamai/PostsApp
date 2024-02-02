58e9551a714c75bd74eafa49dfe20f1d4b5808ac
shinuamai <laurad00sandovalg@gmail.com>
2024-02-01 21:28:35 -0500

    coverage 100%

--- src/tests/pages/Home.test.js
+++ src/tests/pages/Home.test.js
@@ -4,8 +4,16 @@ import '@testing-library/jest-dom/extend-expect';
 import { fireEvent, render, waitFor } from '@testing-library/react';
 import Home from '../../pages/Home';
 
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn(),
+}));
+
 describe('Home component', () => {
   test('adds slide-up animation on button click', async () => {
+    const navigateMock = jest.fn();
+    require('react-router-dom').useNavigate.mockImplementation(() => navigateMock);
+
     const { getByText } = render(      
     <Router>
         <Home />
@@ -17,6 +25,7 @@ describe('Home component', () => {
     await waitFor(() => {
       const homeElement = document.getElementById('home');
       expect(homeElement).toHaveClass('slide-up');
-    });
+      expect(navigateMock).toHaveBeenCalledWith('/posts');
+    }, { timeout: 2000 });
   });
 });
